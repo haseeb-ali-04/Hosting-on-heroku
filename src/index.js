1678aa06160d9e048decd71379e3f467d8504761
Haseeb <mujeebali9966@gmail.com>
2021-05-26 21:51:14 +0530

    hosting on heroku

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
+const port = PORT.env.no || 8000
+
+
+const HtmlPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialPath = path.join(__dirname, "../templates/partials")
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+app.use(express.static(HtmlPath))
+
+
+
+
+
+
+app.get("/", (req, res) =>{
+    res.render("index", {
+        owner : "Haseeb"
+    })
+})
+
+app.get("/weather", (req, res)=>{
+    res.render("weather")
+})
+
+app.get("*", (req, res) =>{
+    res.render("404", {
+        ErrorMsg : "Oops! Page doesn't exist"
+    })
+})
+
+app.listen(port,()=>{
+    console.log("running at port "+port);
+})
