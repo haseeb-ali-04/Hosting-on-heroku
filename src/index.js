17eacc2864926498f67e02e1ac05788c2d029eac
Haseeb <mujeebali9966@gmail.com>
2021-05-26 22:01:29 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const hbs = require('hbs')
-const port = PORT.env.no || 8000
+const port = process.env.PORT || 8000
 
 
 const HtmlPath = path.join(__dirname, "../public")
